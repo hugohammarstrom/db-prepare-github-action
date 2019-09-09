2bc79e2ceedfc6f1479a7b45e9d31858c26e2a30
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-09-09 20:27:19 +0200

    add db-prepare

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
-FROM alpine:3.10
+FROM node:12.10.0-alpine
+
+RUN npm i -g @hugohammarstrom/db-prepare
+
+RUN apk update \
+  && apk upgrade \
+  && apk --no-cache add --update software-properties-common
+RUN add-apt-repository ppa:rmescandon/yq
+RUN apk update
+RUN apk add yq
 
 COPY LICENSE README.md /
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,19 @@
 #!/bin/sh -l
 
-echo "hello $1"
+config_file=$1
+host=$2
+installation_path=$3
+ssh_key=$4
+
+version=$(yq read $config_file version)
+dumpfile=$(yq read $config_file dumpfile)
+
+ssh_key=$(echo $ssh_key | base64 -D)
+
+
+
+echo $ssh_key | ssh -i /dev/stdin $host wp db export --path=${installation_path} db-prepare-dump.sql --allow-root
+echo $ssh_key | scp -i /dev/stdin $host:db-prepare-dump.sql $dumpfile
+echo $ssh_key | ssh -i /dev/stdin $host rm db-prepare-dump.sql || true
+
+db-prepare --config $config_file
