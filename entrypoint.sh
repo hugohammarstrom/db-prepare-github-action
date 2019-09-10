2caa80355d3bd857cab9be4d7c1d33dc71860a31
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-09-10 10:02:10 +0200

    add logging of inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ config_file=$1
 host=$2
 installation_path=$3
 
-echo config_file
-echo host
-echo installation_path
+echo "config_file:" $config_file
+echo "host:" $host
+echo "installation_path:" $installation_path
 
 version=$(yq read $config_file version)
 dumpfile=$(yq read $config_file dumpfile)
