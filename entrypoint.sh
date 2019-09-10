041935cdc879da41e341df14e2a7b28fb9957cac
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-09-10 12:18:02 +0200

    change to rsync

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,21 @@ ssh_host=$2
 ssh_user=$3
 installation_path=$4
 
+version=$(yq read $config_file version)
+dumpfile=$(yq read $config_file dumpfile)
+
+echo "\n"
+echo "------------------------------------------------"
+
 echo "config_file:" $config_file
 echo "ssh_host:" $ssh_host
 echo "ssh_user:" $ssh_user
 echo "installation_path:" $installation_path
+echo "version:" $version
+echo "dumpfile:" $dumpfile
 
-version=$(yq read $config_file version)
-dumpfile=$(yq read $config_file dumpfile)
+echo "------------------------------------------------"
+echo "\n"
 
 mkdir -p "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
@@ -32,8 +40,16 @@ chmod 600 ssh_key
 ssh-add ssh_key
 ssh-keyscan -t rsa ${ssh_host} -f ./ssh_key >> "$SSH_PATH/known_hosts"
 
-ssh -i ./ssh_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} ${ssh_user}@${ssh_host} wp db export --path=${installation_path} db-prepare-dump.sql --allow-root
-scp -i ./ssh_key -o StrictHostKeyChecking=no -p ${PORT:-22} ${ssh_user}@${ssh_host}:db-prepare-dump.sql $dumpfile
+rm -rf "$dumpfile" || true
+
+
 ssh -i ./ssh_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} ${ssh_user}@${ssh_host} rm db-prepare-dump.sql || true
+echo "Exporting database"
+ssh -i ./ssh_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} ${ssh_user}@${ssh_host} wp db export --path=${installation_path} db-prepare-dump.sql --allow-root
+echo "Downloading databasedump"
+rsync -chavzP -e "ssh -i ./ssh_key -o StrictHostKeyChecking=no" --stats ${ssh_user}@${ssh_host}:db-prepare-dump.sql ./
+ssh -i ./ssh_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} ${ssh_user}@${ssh_host} rm db-prepare-dump.sql || true
+
+mv db-prepare-dump.sql "$dumpfile"
 
 db-prepare --config $config_file
