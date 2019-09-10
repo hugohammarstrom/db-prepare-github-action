1be113e6c25b8f4d0e109c77f5bfb41bf2a26c78
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-09-10 11:00:08 +0200

    more ssh fixing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,13 @@ set -e
 SSH_PATH="$HOME/.ssh"
 
 config_file=$1
-host=$2
-installation_path=$3
+ssh_host=$2
+ssh_user=$3
+installation_path=$4
 
 echo "config_file:" $config_file
-echo "host:" $host
+echo "ssh_host:" $ssh_host
+echo "ssh_user:" $ssh_user
 echo "installation_path:" $installation_path
 
 version=$(yq read $config_file version)
@@ -24,12 +26,13 @@ chmod 600 "$SSH_PATH/known_hosts"
 eval $(ssh-agent)
 
 ssh_key=$(echo $ssh_key | openssl enc -base64 -d)
+rm -f ssh_key
 echo $ssh_key > ssh_key
 chmod 400 ssh_key
-ssh-keyscan -t rsa $HOST -f ./ssh_key >> "$SSH_PATH/known_hosts"
+ssh-keyscan -t rsa $ssh_host -f ./ssh_key >> "$SSH_PATH/known_hosts"
 
-ssh -i ./ssh_key $host wp db export --path=${installation_path} db-prepare-dump.sql --allow-root
-scp -i ./ssh_key $host:db-prepare-dump.sql $dumpfile
-ssh -i ./ssh_key $host rm db-prepare-dump.sql || true
+ssh -i ./ssh_key $ssh_user@$ssh_host wp db export --path=${installation_path} db-prepare-dump.sql --allow-root
+scp -i ./ssh_key ssh_user@$ssh_host:db-prepare-dump.sql $dumpfile
+ssh -i ./ssh_key ssh_user@$ssh_host rm db-prepare-dump.sql || true
 
 db-prepare --config $config_file
